585522815ec879b012618664c625d45192cb040a
sakroid <sakroid0507@gmail.com>
2025-02-25 19:09:07 +0900

    テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,7 @@ class User < ApplicationRecord
       validates :first_name_kana, presence: true
     end
     validates :birth_date, presence: true
-    
+    PASSWORD_REGEX = /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX, message: 'には半角英字と半角数字の両方を含めて設定してください'
   end
 end
