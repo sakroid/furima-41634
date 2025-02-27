9383bef05253a3326f1d8e884d64f8941c586b43
sakroid <sakroid0507@gmail.com>
2025-02-27 14:18:08 +0900

    商品出品機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,26 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  def new
+    @item = Item.new
+  end
+
+  def index
+    @items = Item.order('created_at DESC')
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :info, :category_id, :status_id, :fee_status_id, :prefecture_id, :schedule_delivery_id,
+                                 :price, :image).merge(user_id: current_user.id)
+  end
 end
