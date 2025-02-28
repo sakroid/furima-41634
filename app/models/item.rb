fc399d877b7441ae0008115fa081aa95f5d695ff
sakroid <sakroid0507@gmail.com>
2025-02-28 15:14:06 +0900

    商品出品機能修正点2

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   has_one_attached :image
-  has_one :purchase
+  # has_one :purchase
   belongs_to :category
   belongs_to :status
   belongs_to :fee_status
