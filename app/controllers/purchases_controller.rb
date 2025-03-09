b47bbce977bb98fa44cb194381d4f718b4db9739
sakroid <sakroid0507@gmail.com>
2025-03-09 13:52:39 +0900

    購入機能微調整

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -9,13 +9,18 @@ class PurchasesController < ApplicationController
   def create
     @purchase_shipping = PurchaseShipping.new(purchase_params)
     @item = Item.find(params[:item_id])
+    if @purchase_shipping.valid?
+      pay_item
+      @purchase_shipping.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
   end
 
   private
 
   def purchase_params
-    params.require(:purchase_shipping).permit(:postal_coad, :prefecture_id, :city, :address, :building, :phone_number).merge(
-      user_id: current_user.id, item_id: @item.id, token: params[:token]
-    )
+    params.require(:purchase_shipping).permit(:postal_coad, :prefecture_id, :city, :address, :building, :phone_number)
   end
 end
